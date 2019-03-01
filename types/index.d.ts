852a257e232877e2f149afa5d3cb40df668f40a1
nocoolyoyo <574857444@qq.com>
2019-03-01 15:34:22 +0800

    fix d.ts wrong used module name

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,58 +1,56 @@
 /**
  * Created by nocoolyoyo on 2018/11/3.
  */
-declare module "@megmore/es-helper" {
-    // deepCopy
-    export function deepCopy(source: any): any
-    // device
-    export function initClient(lang: string, isMount?: boolean): string
+// deepCopy
+export function deepCopy(source: any): any
+// device
+export function initClient(lang: string, isMount?: boolean): string
 
-    export function getClient(): any
+export function getClient(): any
 
-    // imageLoaded
+// imageLoaded
 
-    export function imageLoaded(url: string): Promise<any>
+export function imageLoaded(url: string): Promise<any>
 
-    // urlParams
+// urlParams
 
-    export function getUrlParam(name: string, url?: string): string
+export function getUrlParam(name: string, url?: string): string
 
-    export function getUrlParams(url?: string): string
+export function getUrlParams(url?: string): string
 
-    export function appendUrlParams(params: any, url?: string, sort?: boolean): string
+export function appendUrlParams(params: any, url?: string, sort?: boolean): string
 
-    // easeScroll
-    interface ScrollFunc {
-        ($scroller: HTMLElement,
-         options: {
-             target: HTMLElement | number |  'start' | 'end'
-             position: 'center' |  'start' | 'end'
-             justify ?: number
-             duration ?: number
-             transition ?: 'linear' | 'easeIn' | 'strongEaseIn' | 'strongEaseOut' | 'sineaseIn' | 'sineaseOut'
-         }): Promise<void>,
-    }
-
-    export const scrollToX: ScrollFunc
-
-    export const scrollToY: ScrollFunc
-
-    // styleUtils
-    export function strStyle(val: any): string
-    export function getStyle(dom: HTMLElement, attr: string): string
-    export function camelCase(name: string): string
-
-    // validate
-    export function isTrimEmpty(val: string): boolean
-    export function isEmail(val: string): boolean
-    export function isLowerCase(val: string): boolean
-    export function isUpperCase(val: string): boolean
-    export function isHexColor(val: string): boolean
-    export function isStyleUnit(val: string): boolean
-    export function isURL(val: string): boolean
-    export function isEmptyObject(val: string): boolean
-    export function isOSWindows(): boolean
-    export function isOSIos(): boolean
-    export function isOSAndroid(): boolean
-    export function isMobile(): boolean
+// easeScroll
+interface ScrollFunc {
+    ($scroller: HTMLElement,
+     options: {
+         target: HTMLElement | number |  'start' | 'end'
+         position: 'center' |  'start' | 'end'
+         justify ?: number
+         duration ?: number
+         transition ?: 'linear' | 'easeIn' | 'strongEaseIn' | 'strongEaseOut' | 'sineaseIn' | 'sineaseOut'
+     }): Promise<void>,
 }
+
+export const scrollToX: ScrollFunc
+
+export const scrollToY: ScrollFunc
+
+// styleUtils
+export function strStyle(val: any): string
+export function getStyle(dom: HTMLElement, attr: string): string
+export function camelCase(name: string): string
+
+// validate
+export function isTrimEmpty(val: string): boolean
+export function isEmail(val: string): boolean
+export function isLowerCase(val: string): boolean
+export function isUpperCase(val: string): boolean
+export function isHexColor(val: string): boolean
+export function isStyleUnit(val: string): boolean
+export function isURL(val: string): boolean
+export function isEmptyObject(val: string): boolean
+export function isOSWindows(): boolean
+export function isOSIos(): boolean
+export function isOSAndroid(): boolean
+export function isMobile(): boolean
